c84b59d028d3f383f64dac3c0c61f12615cf8161
Alexander Teno <alexander.teno@vts.com>
2023-09-29 14:15:08 -0400

    chore: style improvements

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,26 @@ function App() {
   }
 
   return (
-    <div>
-      <h1 className="" data-testid="introductory-message">
-        Hello, World
-      </h1>
-
-      <form onSubmit={handleAddNewItem}>
-        <input
-          className="border-solid border-2 border-black"
-          type="text"
-          name="todo-input"
-          data-testid="todo-input"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
-
-      <TODOList todoItems={items} />
+    <div className="w-full">
+      <div className="container max-w-lg mx-auto border p-4">
+        <form onSubmit={handleAddNewItem} className="flex row-auto">
+          <input
+            className=" rounded-lg rounded-r-none h-8 px-4 border-solid border border-black grow"
+            type="text"
+            name="todo-input"
+            data-testid="todo-input"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button
+            className="rounded-lg rounded-l-none border-black border px-4 h-8 border-l-0"
+            type="submit"
+          >
+            Add
+          </button>
+        </form>
+        <TODOList todoItems={items} />
+      </div>
     </div>
   );
 }
--- src/TODOList.tsx
+++ src/TODOList.tsx
@@ -8,9 +8,9 @@ export function TODOList({ todoItems }: Props) {
   }
 
   return (
-    <ul data-testid="todo-list">
+    <ul data-testid="todo-list" className="mt-2 divide-y border divide-black border-black rounded-md">
       {todoItems.map((todoItem, idx) => (
-        <li key={idx} data-testid="todo-item">
+        <li key={idx} data-testid="todo-item" className="px-4 h-8 flex items-center justify-start">
           {todoItem}
         </li>
       ))}
